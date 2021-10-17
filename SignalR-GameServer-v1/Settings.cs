2d5c70f8637ca1712a5b1783926236b7dc651a79
Edgaras Navickas <edgnav@ktu.lt>
2021-10-17 17:11:31 +0300

    singleton

---
+++ SignalR-GameServer-v1/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace SignalR_GameServer_v1
+{
+    public class Settings
+    {
+        private static Settings instance = null;
+
+        public int PlayerCount;
+        public int MapWidth;
+        public int MapHight;
+
+        private Settings()
+        {
+            PlayerCount = 4;
+            MapWidth = 1000;
+            MapHight = 640;
+        }
+
+        public static Settings getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Settings();
+            }
+            return instance;
+        }
+    }
+}
