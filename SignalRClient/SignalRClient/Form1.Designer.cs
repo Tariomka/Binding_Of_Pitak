4bccf77fc4f9f9ff6bd7204cef918b0c8f403532
Tautvizzzzzz <tautvydas.binkauskas@gmail.com>
2021-10-21 00:36:11 +0300

    multiple players
    "It just works"
    -Famous Chess Player

---
+++ SignalRClient/SignalRClient/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace SignalRClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UP = new System.Windows.Forms.Button();
+            this.DOWN = new System.Windows.Forms.Button();
+            this.LEFT = new System.Windows.Forms.Button();
+            this.RIGHT = new System.Windows.Forms.Button();
+            this.messagesList = new System.Windows.Forms.ListBox();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UP
+            // 
+            this.UP.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.UP.Location = new System.Drawing.Point(1034, 502);
+            this.UP.Name = "UP";
+            this.UP.Size = new System.Drawing.Size(128, 44);
+            this.UP.TabIndex = 9;
+            this.UP.Text = "UP";
+            this.UP.UseVisualStyleBackColor = true;
+            this.UP.Click += new System.EventHandler(this.UP_Click);
+            // 
+            // DOWN
+            // 
+            this.DOWN.Location = new System.Drawing.Point(1179, 502);
+            this.DOWN.Name = "DOWN";
+            this.DOWN.Size = new System.Drawing.Size(128, 44);
+            this.DOWN.TabIndex = 10;
+            this.DOWN.Text = "DOWN";
+            this.DOWN.UseVisualStyleBackColor = true;
+            this.DOWN.Click += new System.EventHandler(this.DOWN_Click);
+            // 
+            // LEFT
+            // 
+            this.LEFT.Location = new System.Drawing.Point(1034, 552);
+            this.LEFT.Name = "LEFT";
+            this.LEFT.Size = new System.Drawing.Size(128, 44);
+            this.LEFT.TabIndex = 11;
+            this.LEFT.Text = "LEFT";
+            this.LEFT.UseVisualStyleBackColor = true;
+            this.LEFT.Click += new System.EventHandler(this.LEFT_Click);
+            // 
+            // RIGHT
+            // 
+            this.RIGHT.Location = new System.Drawing.Point(1179, 552);
+            this.RIGHT.Name = "RIGHT";
+            this.RIGHT.Size = new System.Drawing.Size(128, 44);
+            this.RIGHT.TabIndex = 12;
+            this.RIGHT.Text = "RIGHT";
+            this.RIGHT.UseVisualStyleBackColor = true;
+            this.RIGHT.Click += new System.EventHandler(this.RIGHT_Click);
+            // 
+            // messagesList
+            // 
+            this.messagesList.FormattingEnabled = true;
+            this.messagesList.Location = new System.Drawing.Point(1034, 12);
+            this.messagesList.Name = "messagesList";
+            this.messagesList.Size = new System.Drawing.Size(270, 472);
+            this.messagesList.TabIndex = 15;
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.picCanvas.Location = new System.Drawing.Point(12, 12);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(1000, 640);
+            this.picCanvas.TabIndex = 14;
+            this.picCanvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 669);
+            this.Controls.Add(this.messagesList);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.RIGHT);
+            this.Controls.Add(this.LEFT);
+            this.Controls.Add(this.DOWN);
+            this.Controls.Add(this.UP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UP;
+        private System.Windows.Forms.Button DOWN;
+        private System.Windows.Forms.Button LEFT;
+        private System.Windows.Forms.Button RIGHT;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.ListBox messagesList;
+    }
+}
+
---
+++ SignalRClient/SignalRClient/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+using SignalRClient.Map;
+
+namespace SignalRClient
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection;
+        TileFactory grass = new GrassFactory(1);
+        TileFactory lava = new LavaFactory(1);
+        public int mapHeight;
+        public int mapWidth;
+        private Guid uid = Guid.NewGuid();
+        private Dictionary<int, PictureBox> players = new Dictionary<int, PictureBox>();
+        private int playerid = 0;
+        private string playerName => $"Player {playerid}";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44332/GameHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Invoke((Action)(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                }));
+            });
+
+            connection.On<int, string>("ReceiveCoordinates", (pid, direction) =>
+            {
+                movePlayer(pid, direction);
+            });
+
+            connection.On<int, int>("ReceiveMapCoordinates", (x, y) =>
+            {
+                mapWidth = x;
+                mapHeight = y;
+            });
+
+            connection.On<int, Dictionary<string, int>>("NewIdReceived", (id, playersInGame) =>
+            {
+                this.OnNewIdReceived(id, playersInGame);
+            });
+
+            
+            connection.On<int>("PlayerJoined", (id) =>
+            {
+                this.OnNewPlayerJoined(id);
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+
+                await connection.InvokeAsync("JoinGame", this.uid);
+
+                await connection.InvokeAsync("GetMapSize");
+                messagesList.Items.Add("Map size:" + mapWidth + " " + mapHeight);
+
+
+                createMap(mapWidth, mapHeight);
+
+                //connectButton.IsEnabled = false;
+                //sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private void OnNewIdReceived(int id, Dictionary<string, int> playersInGame)
+        {
+            this.playerid = id;
+            AddMessage($"My Player ID is: {this.playerid}");
+            AddMessage($"Players in game: {string.Join(",", playersInGame.Values.ToArray())}");
+            this.GeneratePlayers(playersInGame);
+        }
+
+        private void GeneratePlayers(Dictionary<string, int> playersInGame)
+        {
+            foreach(var id in playersInGame.Values)
+            {
+                this.AddPlayer(id);
+            }
+        }
+
+        private void OnNewPlayerJoined(int id)
+        {
+            this.AddPlayer(id);
+        }
+
+
+        private void AddPlayer(int id)
+        {
+            if (!players.ContainsKey(id))
+                this.players.Add(id, GetNewPlayer(id));
+
+            messagesList.Items.Add($"!! Player ID {this.playerid} has joined the game");
+        }
+
+        private void AddMessage(string message)
+        {
+            messagesList.Items.Add(message);
+        }
+
+
+        
+        private PictureBox GetNewPlayer(int id)
+        {
+            var pb = new PictureBox();
+            //((System.ComponentModel.ISupportInitialize)(pb)).BeginInit();
+            // 
+            pb.BackColor = Color.Transparent;
+            pb.Image = this.playerid == id ? Properties.Resources.player : Properties.Resources.enemy;
+            pb.Location = new Point(480, 320);
+            pb.Name = $"Player{id}";
+            pb.Size = new Size(40, 40);
+            pb.TabIndex = 20 + id;
+            pb.TabStop = false;
+            pb.Visible = true;
+
+            this.Controls.Add(pb);
+            pb.BringToFront();
+            //((System.ComponentModel.ISupportInitialize)(pb)).EndInit();
+            AddMessage($"Player {id} has been added to the map.");
+            return pb;
+        }
+
+        private async void UP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    this.playerName, "move up!");
+                movePlayer(this.playerid, "UP");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void DOWN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    this.playerName, "move down!");
+                movePlayer(this.playerid, "DOWN");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void RIGHT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    this.playerName, "move right!");
+                movePlayer(this.playerid, "RIGHT");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void LEFT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    this.playerName, "move left!");
+                movePlayer(this.playerid, "LEFT");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        //private void sendBoxCoordinatesOld(string direction)
+        //{
+        //    int x = pictureBox1.Location.X;
+        //    int y = pictureBox1.Location.Y;
+        //
+        //    if (direction == "RIGHT") x += 40;
+        //    else if (direction == "LEFT") x -= 40;
+        //    else if (direction == "UP") y -= 40;
+        //    else if (direction == "DOWN") y += 40;
+        //    pictureBox1.Location = new Point(x, y);
+        //    // _ = SendGetCoordinatesAsync(x, y);
+        //}
+
+        private void movePlayer(int pid, string direction)
+        {
+            var player = players[pid];
+            player.BringToFront();
+
+            int x = player.Location.X;
+            int y = player.Location.Y;
+
+            if (direction == "RIGHT") x += 40;
+            else if (direction == "LEFT") x -= 40;
+            else if (direction == "UP") y -= 40;
+            else if (direction == "DOWN") y += 40;
+            player.Location = new Point(x, y);
+
+            // if this is me, let other know that i moved
+            // otherwise just move the piece and don't send message
+            if (pid == this.playerid)
+            {
+                _ = SendGetCoordinatesAsync(this.playerid, direction);
+            }
+        }
+
+        private async Task SendGetCoordinatesAsync(int pid, string direction)
+        {
+            await connection.InvokeAsync("SendCoordinates",
+                    pid, direction);
+        }
+
+        private void createMap(int width, int height)
+        {
+            int a = 5;
+            Random rnd = new Random();
+            for (int i = 0; i < width; i += 40)
+            {
+                for (int j = 0; j < height; j += 40)
+                {
+                    a++;
+                    Tile til = GetTile(rnd.Next(30));
+                    string pictureName = String.Concat("pictureBox", a);
+                    var picture = new PictureBox
+                    {
+                        Name = pictureName,
+                        Size = new Size(40, 40),
+                        Location = new Point(i, j),
+                        Image = Image.FromFile(til.image),
+
+                    };
+                    this.Controls.Add(picture);
+                }
+            }
+            this.picCanvas.SendToBack();
+        }
+
+        private Tile GetTile(int rnd)
+        {
+            if (rnd < 26)
+                return grass.GetTile();
+            else
+                return lava.GetTile();
+        }
+
+    }
+}
